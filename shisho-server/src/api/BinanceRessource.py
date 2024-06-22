eabfbc81eec09f79127c09013f8db5aee457dd07
annaselma <annagrebahsaly@gmail.com>
2024-06-23 00:16:17 +0200

    init api prediction

--- shisho-server/src/api/BinanceRessource.py
+++ shisho-server/src/api/BinanceRessource.py
@@ -1,6 +1,8 @@
 from binance.client import Client
-from flask import Flask, render_template, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from datetime import datetime
+import pickle
 
 app = Flask(__name__)
 CORS(app)
@@ -22,18 +24,56 @@ def retrieveBinanceDataBy():
     })
 @app.route('/api/history')
 def retrieve_history():
-    candlesticks = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1HOUR, "1 Jul, 2023", "12 Jul, 2023")
+    candlesticks = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1HOUR, "1 Janv, 2023", "15 Jun, 2024")
     datas = []
     for data in candlesticks:
+        timestamp = data[0] / 1000  # Convert milliseconds to seconds
+        date_time = datetime.fromtimestamp(timestamp)
         candlesticks = {
-            "time": data[0]/1000,
+            "time": date_time.strftime('%Y-%m-%d %H:%M:%S'),
             "open":data[1],
             "high":data[2],
             "low": data[3],
             "close": data[4]
         }
         datas.append(candlesticks)
+        #json_file = json.loads(jsonify(datas))
+        #write_csv_file(json_file)
     return jsonify(datas)
+
+    # Load the ARIMA model
+def load_arima_model():
+    with open('arima_model.pkl', 'rb') as f:
+        model = pickle.load(f)
+    return model
+
+# Endpoint for making predictions
+@app.route('api/predict', methods=['POST'])
+def predict():
+    # Get JSON input data
+    data = request.get_json()
+
+    # Example: Load time series data (assuming it's provided in the request)
+    # Replace this with your actual data handling logic
+    # For example, you may load data from a file path or database
+    
+    ##time_series_data = df.DataFrame(data['time_series'])
+
+    # Number of steps ahead to forecast
+    steps = data['steps']
+
+    # Load ARIMA model
+    model = load_arima_model()
+
+    # Make predictions
+    forecast = model.forecast(steps)[0]
+
+    # Prepare JSON response
+    response = {
+        'forecast': forecast.tolist()
+    }
+
+    return jsonify(response)
 
 
 # A method that runs the application server.
